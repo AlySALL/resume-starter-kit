6313cc836b1962050a2f84aae7129c483cdc5ba0
Eric Hemmerlin <ehemmerlin@users.noreply.github.com>
2018-06-27 10:46:46 +0200

    fix: test

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 echo 'GET http://localhost:8080' | vegeta attack -rate 100 -duration 1s > /dev/null
 # perform the stress test
 echo 'GET http://localhost:8080' | timeout 7s vegeta attack -rate 100 -duration 5s | vegeta report
-exit ${PIPESTATUS[1]}
+exit "${PIPESTATUS[1]}"
